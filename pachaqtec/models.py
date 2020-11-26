e70e6672c198cfa15bc62a5e2e09df89731307a3
BryanFM <bryan.floresmagallanes@mail.com>
2020-11-26 17:30:22 -0500

    models-update

--- pachaqtec/models.py
+++ pachaqtec/models.py
@@ -13,6 +13,36 @@ class Tipo_Imagen(models.Model):
     #    verbose_name = 'Tipo Imagen'
     #    verbose_name_plural = 'Tipos de Imagen'
 
+class Tipo_Descuento(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.descripcion
+
+class Descuento(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo_descuento_id = models.ForeignKey(Tipo_Descuento, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=20)
+    cupon = models.CharField(max_length=20)
+    importe = models.DecimalField(decimal_places=2)
+    porcentaje = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return self.descripcion
+
+class Descuento_Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    descuento_id = models.ForeignKey(Descuento, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=20)
+    vigencia_inicio = models.DateField()
+    vigencia_fin = models.DateField()
+    estado = models.BooleanField()
+
+    def __str__(self):
+        return self.descripcion
+
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=80)
@@ -79,6 +109,55 @@ class Carrito(models.Model):
     moneda = models.CharField(max_length=3)
     descuento_id = models.DecimalField(decimal_places=2)
     estado = models.BooleanField()
+
+    def __str__(self):
+        return self.total
+
+class Plan_Estudio(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcion
+
+class Unidad(models.Model):
+    unidad_id = models.AutoField(primary_key=True)
+    plan_id = models.ForeignKey(Plan_Estudio, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcion
+
+class Sub_Unidad(models.Model):
+    id = models.AutoField(primary_key=True)
+    unidad_id = models.ForeignKey(Unidad, on_delete=models.CASCADE)
+    nivel = models.CharField(max_length=1)
+    posicion = models.CharField(max_length=1)
+    descripcion = models.CharField(max_length=100)
+    estado = models.BooleanField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Plan_Estudio_Producto(models.Model):
+    plan_producto_id = models.AutoField(primary_key=True)
+    plan_id = models.ForeignKey(Plan_Estudio, on_delete=models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    estado = models.BooleanField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Carrito_detail(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    posicion = models.AutoField(primary_key=True)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
+    cantidad = models.CharField(max_length=1)
+    importe = models.DecimalField(decimal_places=2)
 
     def __str__(self):
         return self.descripcion
