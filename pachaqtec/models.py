96e4e185e2fbc399bdb1caae5e8d635276378b27
Charly Cristian Chinchay Escamilo <charlycris.che@gmail.com>
2020-11-27 00:46:49 -0500

    version hasta producto

--- pachaqtec/models.py
+++ pachaqtec/models.py
@@ -16,17 +16,18 @@ class Tipo_Imagen(models.Model):
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=80)
-    descripcion = models.TextField
+    descripcion = models.TextField()
     descripcion_corta = models.TextField()
     descripcion_larga = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     telefono = models.CharField(max_length=20)
     orientado_a = models.CharField(max_length=255)
     otorga = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, blank=False, null=False)
 
     def __str__(self):
         return f'{self.nombre} {self.descripcion} {self.descripcion_corta} {self.descripcion_larga} {self.precio} {self.telefono} {self.orientado_a} {self.otorga}'
-    
+
     class Meta():
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
@@ -55,6 +56,22 @@ class Plan_Estudio(models.Model):
     class Meta():
         verbose_name = 'Plan de Estudio'
         verbose_name_plural = 'Planes de Estudio'
+
+class Plan_Estudio_Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    plan_id = models.ForeignKey(Plan_Estudio, on_delete=models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=50, blank=True, null=True)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    estado = models.BooleanField(default="X")
+
+    def __str__(self):
+        return self.descripcion
+
+    class Meta():
+        verbose_name = 'Plan de Estudio Producto'
+        verbose_name_plural = 'Planes de Estudio de Producto'
 
 class Unidad_Estudio(models.Model):
     id = models.AutoField(primary_key=True)
--- pachaqtec/views.py
+++ pachaqtec/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
-from .models import Tipo_Imagen, Producto, Imagen_Producto, Plan_Estudio, Unidad_Estudio, Sub_Unidad_Estudio
+from .models import Tipo_Imagen, Producto, Imagen_Producto, Plan_Estudio, Unidad_Estudio, Sub_Unidad_Estudio, Plan_Estudio_Producto
 from .serializers import Tipo_Imagen_Serializer, ProductoSerializer, Imagen_ProductoSerializer, Plan_EstudioSerializer, Unidad_EstudioSerializer, Sub_Unidad_EstudioSerializer
+from rest_framework.response import Response
+from django.core import serializers
 
 class Tipo_Imagen_List(generics.ListCreateAPIView):
     queryset = Tipo_Imagen.objects.all()
@@ -14,9 +16,34 @@ class Producto_List(generics.ListCreateAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
+    def list(self, request):
+        queryset = self.get_queryset()
+        # producto = serializers.serialize("json", queryset)
+        producto = queryset.values()
+        print(queryset.values())
+        prod = []
+        for p in producto:
+            # print(p["nombre"])
+            imagen_producto = Imagen_Producto.objects.filter(producto=p["id"])
+            plan_estudio = Plan_Estudio_Producto.objects.filter(producto_id=p["id"])
+            print(imagen_producto.values()) 
+            print(plan_estudio.values())
+            prod.append({
+                "nombre" : p["nombre"],
+                "alumno" : "nombrealumno",
+                "imagen" : imagen_producto.values(),
+                "planEstudio" : plan_estudio.values()
+            })
+        
+        print(prod)
+        serializer = ProductoSerializer(queryset, many=True)
+        return Response(prod)
+
 class Producto_Detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+    lookup_field = "slug"
+
 
 class Imagen_Producto_List(generics.ListCreateAPIView):
     queryset = Imagen_Producto.objects.all()
